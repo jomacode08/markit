c09a589c2b6d1090c2b1ece2d7685845937586be
Joss Martinez <jomac.me8@gmail.com>
2025-10-28 17:01:09 -0600

    Require GitHub account for gist retrieval
    Added a check in GetGistByIdQueryHandler to ensure the user has a connected GitHub account before retrieving a gist.

--- Backend/markitAPI/markit.Application/Features/Gists/Queries/GetGistByIdQuery.cs
+++ Backend/markitAPI/markit.Application/Features/Gists/Queries/GetGistByIdQuery.cs
@@ -1,6 +1,9 @@
-﻿using markit.Application.Contracts.GitHub;
+﻿using markit.Application.Contracts.Authentication.ExternalLogin;
+using markit.Application.Contracts.GitHub;
 using markit.Application.Features.Gists.Queries.ViewModels;
+using markit.Application.Models.Authentication;
 using markit.Application.Models.Authentication.AppUser;
+using markit.Application.Models.Authentication.Enums;
 using MediatR;
 
 namespace markit.Application.Features.Gists.Queries
@@ -14,15 +17,24 @@ namespace markit.Application.Features.Gists.Queries
     public class GetGistByIdQueryHandler : IRequestHandler<GetGistByIdQuery, GistResponse>
     {
         private readonly IGitHubApiService _gitHubApiService;
+        private readonly IExternalLoginService _externalLoginService;
 
-        public GetGistByIdQueryHandler(IGitHubApiService gitHubApiService)
+        public GetGistByIdQueryHandler(IGitHubApiService gitHubApiService, IExternalLoginService externalLoginService)
         {
             _gitHubApiService = gitHubApiService;
+            _externalLoginService = externalLoginService;
         }
 
         public async Task<GistResponse> Handle(GetGistByIdQuery request, CancellationToken cancellationToken)
         {
             GistResponse response = new();
+            if (!await HasGitHubLogin(request.User))
+            {
+                response.Status = GistResponseStatus.Failure;
+                response.ErrorMessage = "It's necessary to connect your user with a GitHub account.";
+                return response;
+            }
+
             try
             {
                 GistViewModel gist = await _gitHubApiService.GetGistById(request.Id, request.User);
@@ -36,6 +48,13 @@ namespace markit.Application.Features.Gists.Queries
                 response.ErrorMessage = ex.Message;
                 return response;
             }
+        }
+
+        private async Task<bool> HasGitHubLogin(AppUser user)
+        {
+            ExternalSignInMethod? gitHubLogin = (await _externalLoginService.GetByUser(user))
+                .FirstOrDefault(l => l.LoginProvider.Equals(LoginProvider.GitHub));
+            return gitHubLogin?.Configured ?? false;
         }
     }
 }
