d2943dbebd200c707ab7663ae998c35d95375f07
Joss Martinez <jomac.me8@gmail.com>
2026-02-16 11:23:16 -0600

    Refactor(API): Rename "User.RegistrationConfirmed" column
    I've added a new migration to rename the "User.RegistrationConfirmed" column to "Enabled". These changes provide consistency with the new user administration changes.

---
+++ Backend/markitAPI/markit.Infraestructure/Migrations/20260216171438_RenameUserRegistrationConfirmedToEnabled.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace markit.Infraestructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameUserRegistrationConfirmedToEnabled : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "RegistrationConfirmed",
+                schema: "security",
+                table: "Users",
+                newName: "Enabled");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Enabled",
+                schema: "security",
+                table: "Users",
+                newName: "RegistrationConfirmed");
+        }
+    }
+}
