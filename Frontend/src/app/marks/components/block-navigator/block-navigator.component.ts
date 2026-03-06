c04533ae72330d6f5f3468b83f3873a57f1ce682
Joss Martinez <jomac.me8@gmail.com>
2026-03-06 14:10:32 -0600

    Refactor(SPA): Change tooltip labels of BlockNavigatorComponent
    I've changed the static "Go to left" and "Go to right" tooltip labels for dynamic labels that show the previous/next block name given the current input values.

--- Frontend/src/app/marks/components/block-navigator/block-navigator.component.ts
+++ Frontend/src/app/marks/components/block-navigator/block-navigator.component.ts
@@ -1,5 +1,6 @@
-import { ChangeDetectionStrategy, Component, EventEmitter, input, Output } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, EventEmitter, input, Output } from '@angular/core';
 import { TooltipModule } from 'primeng/tooltip';
+import { Block } from '../../interfaces/block';
 
 @Component({
   selector: 'block-navigator',
@@ -11,8 +12,21 @@ import { TooltipModule } from 'primeng/tooltip';
 })
 export class BlockNavigatorComponent {
   public blockIndex = input.required<number>();
-  public blocksLength = input.required<number>();
+  public blocks = input.required<Block[]>();
   @Output() public positionChanged = new EventEmitter<number>();
+
+  protected previousBlockName = computed<string>(() => {
+    const currentIndex = this.blockIndex();
+    if (currentIndex === 0) return "";
+    return this.blocks()[currentIndex - 1].title;
+  });
+
+  protected nextBlockName = computed<string>(() => {
+    const currentIndex = this.blockIndex();
+    const blocks = this.blocks();
+    if (currentIndex >= blocks.length - 1) return "";
+    return blocks[currentIndex + 1].title;
+  });
 
   public onNavigateBackwardBtnClick(): void {
     if (this.blockIndex() === 0) return;
@@ -21,7 +35,7 @@ export class BlockNavigatorComponent {
   }
 
   public onNavigateForwardBtnClick(): void {
-    if (this.blockIndex() === this.blocksLength() - 1) return;
+    if (this.blockIndex() === this.blocks().length - 1) return;
     const newIndex = this.blockIndex() + 1;
     this.positionChanged.emit(newIndex);
   }
