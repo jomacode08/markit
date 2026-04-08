7a732205e0af78200f016cd881ee128d3c88381e
Joss Martinez <jomac.me8@gmail.com>
2026-04-07 20:53:24 -0600

    Refactor(SPA): Replace favicon and svg logos

--- Frontend/src/app/shared/styles/theme/custom-preset.ts
+++ Frontend/src/app/shared/styles/theme/custom-preset.ts
@@ -35,7 +35,7 @@ export const CustomPreset = definePreset(Aura, {
                     950: '{orange.950}'
                 },
                 secondary: {
-                    50: '{ blue.50}',
+                    50: '{blue.50}',
                     100: '{blue.100}',
                     200: '{blue.200}',
                     300: '{blue.300}',
@@ -46,6 +46,19 @@ export const CustomPreset = definePreset(Aura, {
                     800: '{blue.800}',
                     900: '{blue.900}',
                     950: '{blue.950}'
+                },
+                contrast: {
+                    50: '{amber.50}',
+                    100: '{amber.100}',
+                    200: '{amber.200}',
+                    300: '{amber.300}',
+                    400: '{amber.400}',
+                    500: '{amber.500}',
+                    600: '{amber.600}',
+                    700: '{amber.700}',
+                    800: '{amber.800}',
+                    900: '{amber.900}',
+                    950: '{amber.950}'
                 },
                 surface: {
                     0: '#ffffff',
@@ -122,6 +135,19 @@ export const CustomPreset = definePreset(Aura, {
                     950: '{violet.950}'
                 },
                 secondary: {
+                    50: '{indigo.50}',
+                    100: '{indigo.100}',
+                    200: '{indigo.200}',
+                    300: '{indigo.300}',
+                    400: '{indigo.400}',
+                    500: '{indigo.500}',
+                    600: '{indigo.600}',
+                    700: '{indigo.700}',
+                    800: '{indigo.800}',
+                    900: '{indigo.900}',
+                    950: '{indigo.950}'
+                },
+                contrast: {
                     50: '{indigo.50}',
                     100: '{indigo.100}',
                     200: '{indigo.200}',
