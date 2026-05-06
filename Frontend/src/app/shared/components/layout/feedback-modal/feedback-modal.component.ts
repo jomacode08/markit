d1c1940e44dc4e952f09ef918077cf78e8c63c22
Joss Martinez <jomac.me8@gmail.com>
2026-05-05 18:32:20 -0600

    Refactor(SPA): Remove contact email config, use author website
    Remove reliance on CONTACT_EMAIL across CI and frontend and replace it with a static author website. This simplifies configuration by removing the contact email variable and displaying the author's website directly in the UI.

--- Frontend/src/app/shared/components/layout/feedback-modal/feedback-modal.component.ts
+++ Frontend/src/app/shared/components/layout/feedback-modal/feedback-modal.component.ts
@@ -1,5 +1,4 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { environment } from '../../../../../environments/environment';
 
 @Component({
     selector: 'app-feedback-modal',
@@ -9,5 +8,5 @@ import { environment } from '../../../../../environments/environment';
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class FeedbackModalComponent {
-  protected readonly CONTACT_EMAIL: string = environment.contactEmail;
+  protected readonly AUTHOR_WEBSITE: string = 'https://www.jomacode.com';
 }
--- Frontend/src/environments/environment.template.ts
+++ Frontend/src/environments/environment.template.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: '${production}', // 'True' or 'False'
   baseApiUrl: '${baseApiUrl}', // ex.'http://localhost:5000'
-  contactEmail: '${contactEmail}', // ex. support@domain.com
 };
