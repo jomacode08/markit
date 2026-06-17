93e5e03b55327d5fbed6110a2d79c0341edf3276
Joss Martinez <jomac.me8@gmail.com>
2026-06-16 20:47:52 -0600

    Refactor(SPA): Update pixel-notepad svg

--- Frontend/src/app/shared/components/ui/pixel-notepad/pixel-notepad.component.ts
+++ Frontend/src/app/shared/components/ui/pixel-notepad/pixel-notepad.component.ts
@@ -5,32 +5,21 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   imports: [],
   template: `
     <div class="noty-container">
-      <svg
-        class="noty-svg"
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 32 32"
-        role="img"
-        aria-label="Notepad illustration"
-      >
+      <svg class="noty-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
+        <title>computer-old-electronics</title>
         <g>
-          <path d="m12.955 0 0 1.52 -3.05 0 0 1.53 3.05 0 0 1.52 1.52 0 0 -1.52 3.05 0 0 1.52 1.52 0 0 -1.52 3.05 0 0 1.52 1.53 0 0 -1.52 1.52 0 0 3.05 1.52 0 0 -1.53 1.53 0 0 25.91 -16.76 0 0 1.52 18.28 0 0 -1.52 1.52 0 0 -25.91 -1.52 0 0 -1.52 -1.52 0 0 -1.53 -4.57 0 0 -1.52 -1.53 0 0 1.52 -3.05 0 0 -1.52 -1.52 0 0 1.52 -3.05 0 0 -1.52 -1.52 0z" fill="currentColor"/>
-          <path d="M23.625 6.1h1.52v3.04h-1.52Z" fill="currentColor"/>
-          <path d="m22.095 12.19 4.57 0 0 -1.52 -3.04 0 0 -1.53 -1.53 0 0 3.05z" fill="currentColor"/>
-          <path d="m20.575 15.24 -1.53 0 0 3.05 1.53 0 0 -1.53 6.09 0 0 -1.52 -4.57 0 0 -3.05 -1.52 0 0 3.05z" fill="currentColor"/>
-          <path d="m17.525 21.33 9.14 0 0 -1.52 -7.62 0 0 -1.52 -1.52 0 0 3.04z" fill="currentColor"/>
-          <path d="M16.005 12.19h1.52v1.53h-1.52Z" fill="currentColor"/>
-          <path d="M16.005 9.14h1.52v1.53h-1.52Z" fill="currentColor"/>
-          <path d="M12.955 24.38h13.71v1.53h-13.71Z" fill="currentColor"/>
-          <path d="M11.435 13.72h4.57v1.52h-4.57Z" fill="currentColor"/>
-          <path d="M11.435 9.14h1.52v1.53h-1.52Z" fill="currentColor"/>
-          <path d="m11.435 22.86 6.09 0 0 -1.53 -15.24 0 0 1.53 7.62 0 0 7.62 1.53 0 0 -7.62z" fill="currentColor"/>
-          <path d="M9.905 12.19h1.53v1.53h-1.53Z" fill="currentColor"/>
-          <path d="M8.385 3.05h1.52V6.1h-1.52Z" fill="currentColor"/>
-          <path d="M6.855 6.1h1.53v3.04h-1.53Z" fill="currentColor"/>
-          <path d="M5.335 9.14h1.52v3.05h-1.52Z" fill="currentColor"/>
-          <path d="M3.815 12.19h1.52v3.05h-1.52Z" fill="currentColor"/>
-          <path d="M2.285 15.24h1.53v3.05h-1.53Z" fill="currentColor"/>
-          <path d="M0.765 18.29h1.52v3.04H0.765Z" fill="currentColor"/>
+          <path d="M30.475 22.86h-4.57v-1.53h1.52v-1.52H4.575v1.52H6.1v1.53H1.525V32h28.95Zm-22.86 -1.53h16.76v1.53H7.615Zm21.34 9.15H3.045v-6.1h25.91Z" fill="currentColor" stroke-width="1"></path>
+          <path d="M27.425 1.52h1.53v18.29h-1.53Z" fill="currentColor" stroke-width="1"></path>
+          <path d="M18.285 25.9h7.62v1.53h-7.62Z" fill="currentColor" stroke-width="1"></path>
+          <path d="M18.285 10.67h1.52v1.52h-1.52Z" fill="currentColor" stroke-width="1"></path>
+          <path d="M18.285 7.62h1.52v1.52h-1.52Z" fill="currentColor" stroke-width="1"></path>
+          <path d="M13.715 12.19h4.57v1.52h-4.57Z" fill="currentColor" stroke-width="1"></path>
+          <path d="M12.185 10.67h1.53v1.52h-1.53Z" fill="currentColor" stroke-width="1"></path>
+          <path d="M12.185 7.62h1.53v1.52h-1.53Z" fill="currentColor" stroke-width="1"></path>
+          <path d="M6.1 18.29h19.81V3.05H6.1ZM7.615 4.57h16.76v12.19H7.615Z" fill="currentColor" stroke-width="1"></path>
+          <path d="M6.095 25.9h3.05v3.05h-3.05Z" fill="currentColor" stroke-width="1"></path>
+          <path d="M4.575 0h22.85v1.52H4.575Z" fill="currentColor" stroke-width="1"></path>
+          <path d="M3.045 1.52h1.53v18.29h-1.53Z" fill="currentColor" stroke-width="1"></path>
         </g>
       </svg>
     </div>
